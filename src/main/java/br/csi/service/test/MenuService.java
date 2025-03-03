56e40b709e70a70716929b114c7f2864203e78c5
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-03 12:38:33 -0300

    refactor: Criando classe ChargeDataUtil
    Criando ChargeDataUtil para limpar o código

--- src/main/java/br/csi/service/test/MenuService.java
+++ src/main/java/br/csi/service/test/MenuService.java
@@ -1,9 +1,6 @@
 package br.csi.service.test;
 
-import br.csi.dao.MenuCategoryDAO;
-import br.csi.dao.MenuDAO;
-import br.csi.entity.Menu;
-import br.csi.entity.MenuCategory;
+import br.csi.utils.ChargeDataUtil;
 import br.csi.utils.JPAUtil;
 
 import javax.persistence.EntityManager;
@@ -12,45 +9,9 @@ import java.math.BigDecimal;
 public class MenuService {
     public static void main(String[] args) {
         EntityManager em = JPAUtil.getEntityManagerRasFood();
-        registerItensMenu(em, registerCategory(em));
-    }
-
-    private static MenuCategory registerCategory(EntityManager em) {
-        MenuCategoryDAO menuCategoryDAO = new MenuCategoryDAO(em);
-        MenuCategory mainDish = new MenuCategory("Main Dish");
         em.getTransaction().begin();
-        em.persist(mainDish);
-        menuCategoryDAO.save(mainDish);
-        em.getTransaction().commit();
-        em.clear();
-
-        return mainDish;
-    }
-
-    private static void registerItensMenu(EntityManager em, MenuCategory category) {
-        Menu risoto = new Menu();
-        risoto.setName("Risoto de frutos do mar");
-        risoto.setDescription("Risoto acompanhado de lula, polvo e mariscos");
-        risoto.setStatus(true);
-        risoto.setPrice(BigDecimal.valueOf(88.50));
-
-        Menu salmao = new Menu();
-        salmao.setName("Salmão ao molho de maracujá");
-        salmao.setDescription("Salmão grelhado ao molho de maracujá");
-        salmao.setStatus(true);
-        salmao.setPrice(BigDecimal.valueOf(60.00));
-
-        MenuDAO menuDAO = new MenuDAO(em);
-        em.getTransaction().begin();
-
-        menuDAO.save(risoto);
-        risoto.setMenuCategory(category);
-        em.flush();
-        menuDAO.save(salmao);
-        salmao.setMenuCategory(category);
-        em.flush();
-        System.out.println("O prato consultado foi: " + menuDAO.findById(1));
-        System.out.println("O prato consultado foi: " + menuDAO.findById(2));
+        ChargeDataUtil.registerCategory(em);
+        ChargeDataUtil.registerProductMenu(em);
         em.close();
     }
 }
--- src/main/java/br/csi/utils/ChargeDataUtil.java
+++ src/main/java/br/csi/utils/ChargeDataUtil.java
@@ -1,4 +1,65 @@
 package br.csi.utils;
 
+import br.csi.dao.MenuCategoryDAO;
+import br.csi.dao.MenuDAO;
+import br.csi.entity.Menu;
+import br.csi.entity.MenuCategory;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ChargeDataUtil {
+    private ChargeDataUtil() {}
+
+    public static void registerCategory(EntityManager em) {
+        MenuCategory salad = new MenuCategory("Saladas");
+        MenuCategory entry = new MenuCategory("Entradas");
+        MenuCategory main = new MenuCategory("Pratos Principais");
+
+        MenuCategoryDAO menuCategoryDAO = new MenuCategoryDAO(em);
+
+        menuCategoryDAO.save(entry);
+        em.flush();
+        menuCategoryDAO.save(salad);
+        em.flush();
+        menuCategoryDAO.save(main);
+        em.flush();
+        em.clear();
+    }
+
+    public static void registerProductMenu(EntityManager em) {
+        MenuDAO menuDAO = new MenuDAO(em);
+        MenuCategoryDAO menuCategoryDAO = new MenuCategoryDAO(em);
+
+        List<MenuCategory> categories = menuCategoryDAO.findAll();
+
+        // Pratos Principais
+        Menu moqueca = new Menu("Moqueca", "Peixe branco, banana da terra, arroz e farofa", true, BigDecimal.valueOf(95.00), LocalDateTime.now(), categories.get(2));
+        Menu feijoada = new Menu("Feijoada", "Feijão preto com linguiça, carne seca, costelinha e acompanhamentos", true, BigDecimal.valueOf(85.00), LocalDateTime.now(), categories.get(2));
+        Menu boboDeCamarao = new Menu("Bobó de Camarão", "Camarão refogado com purê de mandioca e arroz", true, BigDecimal.valueOf(90.00), LocalDateTime.now(), categories.get(2));
+        Menu acaraje = new Menu("Acarajé", "Bolinho de feijão-fradinho recheado com vatapá e camarão", true, BigDecimal.valueOf(45.00), LocalDateTime.now(), categories.get(2));
+
+        // Saladas
+        Menu caprese = new Menu("Salada Caprese", "Tomate, mussarela de búfala, manjericão e azeite", true, BigDecimal.valueOf(35.00), LocalDateTime.now(), categories.get(0));
+        Menu caesar = new Menu("Salada Caesar", "Alface romana, croutons, parmesão e molho Caesar", true, BigDecimal.valueOf(40.00), LocalDateTime.now(), categories.get(0));
+
+        // Entradas
+        Menu bolinhoDeBacalhau = new Menu("Bolinho de Bacalhau", "Bolinho de bacalhau com molho tártaro", true, BigDecimal.valueOf(30.00), LocalDateTime.now(), categories.get(1));
+        Menu pastelDeQueijo = new Menu("Pastel de Queijo", "Pastel frito recheado com queijo derretido", true, BigDecimal.valueOf(25.00), LocalDateTime.now(), categories.get(1));
+
+        // Salvando os pratos no banco de dados
+        menuDAO.save(moqueca);
+        menuDAO.save(feijoada);
+        menuDAO.save(boboDeCamarao);
+        menuDAO.save(acaraje);
+        menuDAO.save(caprese);
+        menuDAO.save(caesar);
+        menuDAO.save(bolinhoDeBacalhau);
+        menuDAO.save(pastelDeQueijo);
+
+        em.getTransaction().commit();
+        em.close();
+    }
 }
