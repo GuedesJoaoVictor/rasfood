6aa12f41d5cc0ab70689ffb100004f41c9be6f83
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-03 22:28:31 -0300

    fix: Removendo localdatetime desnecessario

--- src/main/java/br/csi/utils/ChargeDataUtil.java
+++ src/main/java/br/csi/utils/ChargeDataUtil.java
@@ -36,18 +36,18 @@ public class ChargeDataUtil {
         List<MenuCategory> categories = menuCategoryDAO.findAll();
 
         // Pratos Principais
-        Menu moqueca = new Menu("Moqueca", "Peixe branco, banana da terra, arroz e farofa", true, BigDecimal.valueOf(95.00), LocalDateTime.now(), categories.get(2));
-        Menu feijoada = new Menu("Feijoada", "Feijão preto com linguiça, carne seca, costelinha e acompanhamentos", true, BigDecimal.valueOf(85.00), LocalDateTime.now(), categories.get(2));
-        Menu boboDeCamarao = new Menu("Bobó de Camarão", "Camarão refogado com purê de mandioca e arroz", true, BigDecimal.valueOf(90.00), LocalDateTime.now(), categories.get(2));
-        Menu acaraje = new Menu("Acarajé", "Bolinho de feijão-fradinho recheado com vatapá e camarão", true, BigDecimal.valueOf(45.00), LocalDateTime.now(), categories.get(2));
+        Menu moqueca = new Menu("Moqueca", "Peixe branco, banana da terra, arroz e farofa", true, BigDecimal.valueOf(95.00), categories.get(2));
+        Menu feijoada = new Menu("Feijoada", "Feijão preto com linguiça, carne seca, costelinha e acompanhamentos", true, BigDecimal.valueOf(85.00), categories.get(2));
+        Menu boboDeCamarao = new Menu("Bobó de Camarão", "Camarão refogado com purê de mandioca e arroz", true, BigDecimal.valueOf(90.00), categories.get(2));
+        Menu acaraje = new Menu("Acarajé", "Bolinho de feijão-fradinho recheado com vatapá e camarão", true, BigDecimal.valueOf(45.00), categories.get(2));
 
         // Saladas
-        Menu caprese = new Menu("Salada Caprese", "Tomate, mussarela de búfala, manjericão e azeite", true, BigDecimal.valueOf(35.00), LocalDateTime.now(), categories.get(0));
-        Menu caesar = new Menu("Salada Caesar", "Alface romana, croutons, parmesão e molho Caesar", true, BigDecimal.valueOf(40.00), LocalDateTime.now(), categories.get(0));
+        Menu caprese = new Menu("Salada Caprese", "Tomate, mussarela de búfala, manjericão e azeite", true, BigDecimal.valueOf(35.00), categories.get(0));
+        Menu caesar = new Menu("Salada Caesar", "Alface romana, croutons, parmesão e molho Caesar", true, BigDecimal.valueOf(40.00), categories.get(0));
 
         // Entradas
-        Menu bolinhoDeBacalhau = new Menu("Bolinho de Bacalhau", "Bolinho de bacalhau com molho tártaro", true, BigDecimal.valueOf(30.00), LocalDateTime.now(), categories.get(1));
-        Menu pastelDeQueijo = new Menu("Pastel de Queijo", "Pastel frito recheado com queijo derretido", true, BigDecimal.valueOf(25.00), LocalDateTime.now(), categories.get(1));
+        Menu bolinhoDeBacalhau = new Menu("Bolinho de Bacalhau", "Bolinho de bacalhau com molho tártaro", true, BigDecimal.valueOf(30.00), categories.get(1));
+        Menu pastelDeQueijo = new Menu("Pastel de Queijo", "Pastel frito recheado com queijo derretido", true, BigDecimal.valueOf(25.00), categories.get(1));
 
         // Salvando os pratos no banco de dados
         menuDAO.save(moqueca);
