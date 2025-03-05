8a17b555973b534361f4c925aa25d93860caf1e7
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-05 17:45:41 -0300

    feat: Criando classe OrderService
    Criando classe para manipular o banco de dados.

--- src/main/java/br/csi/service/test/OrderService.java
+++ src/main/java/br/csi/service/test/OrderService.java
@@ -1,4 +1,33 @@
 package br.csi.service.test;
 
+import br.csi.dao.ClientDAO;
+import br.csi.dao.MenuDAO;
+import br.csi.dao.OrderDAO;
+import br.csi.entity.Client;
+import br.csi.entity.Order;
+import br.csi.entity.OrderMenu;
+import br.csi.utils.ChargeDataUtil;
+import br.csi.utils.JPAUtil;
+
+import javax.persistence.EntityManager;
+
 public class OrderService {
+    public static void main(String[] args) {
+        EntityManager em = JPAUtil.getEntityManagerRasFood();
+        em.getTransaction().begin();
+        ChargeDataUtil.registerCategory(em);
+        ChargeDataUtil.registerProductMenu(em);
+
+        MenuDAO menuDAO = new MenuDAO(em);
+        ClientDAO clientDAO = new ClientDAO(em);
+        OrderDAO orderDAO = new OrderDAO(em);
+
+        Client guedes = new Client("Guedes", "05265294059", "970431920");
+        Order order = new Order(guedes);
+        order.setOrder(new OrderMenu(order, menuDAO.findById(1), 2));
+        clientDAO.save(guedes);
+        orderDAO.save(order);
+        em.getTransaction().commit();
+        em.close();
+    }
 }
